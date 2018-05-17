89680f57ebcc625c3ad0c4119fd0a803304ed1c4
wojciechKogut <wkgt33@gmail.com>
2018-05-17 16:57:26 +0200

    refactor code

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,16 +2,14 @@
 
 
 
-class Pages extends Controller
-{
+class Pages extends Controller {
     
     public $posts;
     public $categories;
     public $user;
     public $comment;
     
-    public function __construct()
-    {
+    public function __construct() {
         $this->posts      = $this->model('post');
         $this->categories = $this->model('category');
         $this->user       = $this->model('user');
@@ -20,34 +18,23 @@ class Pages extends Controller
     }
     
     
-
 //    gdy url jest pusty to wczytujemy strone startowa, metoda index jest domyslna metoda gdy nie podamy w url nazwy metody
-    public function index($id = 1)
-    {  
+    public function index($id = 1) {  
                 $all_posts = $this->posts->get_data()->where("post_status","published");
                 $count_rows = $all_posts->count();
                 //id = aktualna strona
                 $pagination = new Pagination(POSTS_PER_PAGE, $id, $count_rows);
-                //ostatni argument pozwali na polaczenie tabel post i category
                 $pager = new Pager(POSTS_PER_PAGE,$this->posts,$id);
                 $posts_per_page = $pager->data_per_page();
-                
                 $recent_posts = $this->posts::orderBy('id','desc')->where("post_status","published")->limit(5)->get();
-                
                 $data = [$posts_per_page, $count_rows, $id, $this->categories->get_data(),$pagination,$recent_posts];
                 $this->view('home', $data);
     }
     
 
-    
-//    gdy url = pages/admin wczytujemy view  -  admin
-    public function admin()
-    {
-
+    public function admin() {
         $session = $this->model('session');
-
-        if ($session->session_check())
-        {
+        if ($session->session_check()) {
             $data = [
                         $this->user->get_data()->count(),
                         $this->posts->get_data()->count(),
@@ -56,12 +43,10 @@ class Pages extends Controller
                         $this->user->subscribers()->count(),
                         $this->posts->active_posts()->count(),
                         $this->posts->draft_posts()->count(),
-
                     ];
             $this->view('admin', $data);
         }
-        else
-        {
+        else {
             redirect(ROOT);
         }
     }
